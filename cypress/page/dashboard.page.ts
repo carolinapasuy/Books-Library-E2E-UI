28541f35732e94d0d740ad1be633fd14e4b57b43
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2023-04-11 09:50:52 -0500

    added verifyBookTitleAndAuthor method, removed unnecesary variable repetition (#7)

--- cypress/page/dashboard.page.ts
+++ cypress/page/dashboard.page.ts
@@ -1,57 +1,77 @@
 class DashboardPage {
-    private addBookButton: string;
-    private deleteBookButton: string;
-    private rowsTable: string;
-    private paginationSizeChanger: string;
-    private paginationSizeOption: string;
-    private firstPageButton: string;
-    private secondPageButton: string;
-    private thirdPageButton: string;
+  private addBookButton: string;
+  private deleteBookButton: string;
+  private editBookButton: string;
+  private booksForPageButton: string;
+  private rowsTable: string;
+  private pageButton: string;
+  private paginationSizeChanger: string;
+  private paginationSizeOption: string;
 
-    constructor() {
-        this.addBookButton ='.ant-btn-primary > .ng-star-inserted:contains("Add")';
-        this.deleteBookButton = '[nztype="default"]';
-        this.paginationSizeChanger ='.ng-star-inserted > .ant-pagination-options.ng-star-inserted > .ant-select.ant-pagination-options-size-changer';
-        this.paginationSizeOption = '.ant-select-item > .ant-select-item-option-content';
-        this.rowsTable = '.ant-table-tbody > .ant-table-row.ng-star-inserted';    
-        this.firstPageButton ='.ng-star-inserted > .ant-pagination-item.ng-star-inserted > .ng-star-inserted:contains("1")';
-        this.secondPageButton ='.ng-star-inserted > .ant-pagination-item.ng-star-inserted > .ng-star-inserted:contains("2")';
-        this.thirdPageButton ='.ng-star-inserted > .ant-pagination-item.ng-star-inserted > .ng-star-inserted:contains("3")';
-    }
+  constructor() {
+    this.addBookButton = '.ant-btn-primary > .ng-star-inserted:contains("Add")';
+    this.deleteBookButton =
+      'div[_ngcontent-uqy-c73] > .ant-btn-primary > .ng-star-inserted:contains("Delete")';
+    this.booksForPageButton =
+      '.ant-select-selection-item.ng-star-inserted[ng-reflect-label="50 / page"]';
+    this.rowsTable = ".ant-table-tbody > .ant-table-row.ng-star-inserted";
+    this.editBookButton = "button.ant-btn-primary.ant-btn-circle";
+    this.pageButton =
+      '.ng-star-inserted > .ant-pagination-item.ng-star-inserted > .ng-star-inserted';
+    this.paginationSizeChanger =
+      '.ng-star-inserted > .ant-pagination-options.ng-star-inserted > .ant-select.ant-pagination-options-size-changer';
+    this.paginationSizeOption = 
+      '.ant-select-item > .ant-select-item-option-content';
+  }
 
-    public addBook() {
-        cy.get(this.addBookButton).click();
-    }
-    public deleteButton() {
-        return cy.get(this.deleteBookButton).click();
-    }
-    public changePaginationSize(size: string) {
-        cy.get(this.paginationSizeChanger).click();
-        const newSize = this.paginationSizeOption + `:contains("${size}")`;
-        cy.get(newSize).click();
-      }
+  public addBook() {
+    cy.get(this.addBookButton).click();
+  }
+  
+  public deleteButton() {
+    return cy.get(this.deleteBookButton).click();
+  }
+  
+  public getBooksForPageButton() {
+    return cy.get(this.booksForPageButton).click();
+  }
 
-    public getRowsTable() {
-        return cy.get(this.rowsTable);
-    }
-    public changePage(number: number) {
-        if (number === 1) {
-          return cy.get(this.firstPageButton).click();
-        } else if (number === 2) {
-          return cy.get(this.secondPageButton).click();
-        } else if (number === 3) {
-          return cy.get(this.thirdPageButton).click();
-        }
-    
-        return null;
-      }
-    public verifyBookTitleAndAuthor(title: string, author: string){  
-        this.getRowsTable()
-        .get(".ant-table-cell")
-        .should("contain", title)
-        .and("contain", author);
-    }
+  public getRowsTable() {
+    return cy.get(this.rowsTable);
+  }
+  
+  public getBookToEditFirstButton() {
+    cy.get(this.editBookButton).eq(0).click();
+  }
+  
+  public changePaginationSize(size: string) {
+    cy.get(this.paginationSizeChanger).click();
+    const newSize = this.paginationSizeOption + `:contains("${size}")`;
+    cy.get(newSize).click();
+  }
 
+  public getRowsTable() {
+    return cy.get(this.rowsTable);
+  }
+
+  public verifyBookTitleAndAuthor(title: string, author: string){  
+    this.getRowsTable()
+    .get(".ant-table-cell")
+    .should("contain", title)
+    .and("contain", author);
+  }
+
+  public changePage(number: number) {
+    const pageToChange= this.pageButton + `:contains("${number}")`;
+    return cy.get(pageToChange).click();
+  }
+
+  public changePaginationSize(size: string) {
+    cy.get(this.paginationSizeChanger).click();
+    const newSize = this.paginationSizeOption + `:contains("${size}")`;
+    cy.get(newSize).click();
+  }
+  
     public verifyBookInDashboard(title: string){   
         this.getRowsTable()
         .contains('td', title)
@@ -59,6 +79,7 @@ class DashboardPage {
         .find('[type="checkbox"]')
         .check();
     }
+    
     public clickEditBook(title: string){   
         this.getRowsTable()
         .contains('td', title)
@@ -71,7 +92,6 @@ class DashboardPage {
         return cy.get(this.rowsTable)
         .eq(0);
     }
-
 }
 
 export { DashboardPage };
