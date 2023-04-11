591673fa1426e0b7ca83144e9f0b3a2f3a4cdd70
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2023-04-10 23:54:05 -0500

    added verifyBookTitleAndAuthor method, removed unnecesary variable repetition

--- cypress/page/dashboard.page.ts
+++ cypress/page/dashboard.page.ts
@@ -4,9 +4,7 @@ class DashboardPage {
   private editBookButton: string;
   private booksForPageButton: string;
   private rowsTable: string;
-  private firstPageButton: string;
-  private secondPageButton: string;
-  private thirdPageButton: string;
+  private pageButton: string;
   private paginationSizeChanger: string;
   private paginationSizeOption: string;
 
@@ -18,12 +16,8 @@ class DashboardPage {
       '.ant-select-selection-item.ng-star-inserted[ng-reflect-label="50 / page"]';
     this.rowsTable = ".ant-table-tbody > .ant-table-row.ng-star-inserted";
     this.editBookButton = "button.ant-btn-primary.ant-btn-circle";
-    this.firstPageButton =
-      '.ng-star-inserted > .ant-pagination-item.ng-star-inserted > .ng-star-inserted:contains("1")';
-    this.secondPageButton =
-      '.ng-star-inserted > .ant-pagination-item.ng-star-inserted > .ng-star-inserted:contains("2")';
-    this.thirdPageButton =
-      '.ng-star-inserted > .ant-pagination-item.ng-star-inserted > .ng-star-inserted:contains("3")';
+    this.pageButton =
+      '.ng-star-inserted > .ant-pagination-item.ng-star-inserted > .ng-star-inserted';
     this.paginationSizeChanger =
       '.ng-star-inserted > .ant-pagination-options.ng-star-inserted > .ant-select.ant-pagination-options-size-changer';
     this.paginationSizeOption = 
@@ -54,21 +48,22 @@ class DashboardPage {
   }
 
   public changePage(number: number) {
-    if (number === 1) {
-      return cy.get(this.firstPageButton).click();
-    } else if (number === 2) {
-      return cy.get(this.secondPageButton).click();
-    } else if (number === 3) {
-      return cy.get(this.thirdPageButton).click();
-    }
-
-    return null;
+    const pageToChange= this.pageButton + `:contains("${number}")`;
+    return cy.get(pageToChange).click();
   }
 
   public changePaginationSize(size: string) {
     cy.get(this.paginationSizeChanger).click();
     const newSize = this.paginationSizeOption + `:contains("${size}")`;
     cy.get(newSize).click();
+  }
+
+  public verifyBookTitleAndAuthor(title: string, author: string){
+    
+          this.getRowsTable()
+          .get(".ant-table-cell")
+          .should("contain", title)
+          .and("contain", author);
   }
 }
 
