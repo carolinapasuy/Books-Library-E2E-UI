cdcb36418f0aba10892e8cbd58a2062ee716f033
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2023-04-11 13:09:23 -0500

    Fixed flaky tests (#11)

--- cypress/e2e/read-book.cy.ts
+++ cypress/e2e/read-book.cy.ts
@@ -8,38 +8,46 @@ describe("Verifying read book process for Library dashboard", () => {
     dashboardPage = new DashboardPage();
   });
 
-    describe("Negative cases", () => {
-      it("Non existent book reading", () => {
-        dashboardPage
-          .getRowsTable()
-          .get(".ant-table-cell")
-          .should("not.contain", "Tecnicas de evaluación de calidad de software")
-          .and("not.contain", "Holmes Giovanny");
-      });
+  describe("Negative cases", () => {
+    it("Non existent book reading", () => {
+      dashboardPage.changePaginationSize("50 / page");
+      dashboardPage
+        .getRowsTable()
+        .get(".ant-table-cell")
+        .should("not.contain", "Tecnicas de evaluación de calidad de software")
+        .and("not.contain", "Holmes Giovanny");
     });
+  });
 
   describe("Positive cases", () => {
-    it("Reading first book on page 1", () => {
-        dashboardPage.verifyBookTitleAndAuthor("23 otoños antes de ti","Elísabet Benavent");
-
+    it("Reading existent book", () => {
+      dashboardPage.changePaginationSize("50 / page");
+      dashboardPage.verifyBookTitleAndAuthor(
+        "Don't Make Me Think: A Common Sense Approach to Web Usability",
+        "Steve Krug"
+      );
     });
 
-    it("Reading a book on page 1", () => {
-        dashboardPage.verifyBookTitleAndAuthor("Programming Pearls","Jon Bentley");
+    it("Reading another existent book", () => {
+      dashboardPage.changePaginationSize("50 / page");
+      dashboardPage.verifyBookTitleAndAuthor(
+        "Programming Pearls",
+        "Jon Bentley"
+      );
     });
 
-    it("Reading a book on page 3", () => {
-        dashboardPage.changePage(3);
-        dashboardPage.verifyBookTitleAndAuthor("El mapa de los anhelos","Alice Kellen");
+    it("Loads 10 books on pagination size 10", () => {
+      dashboardPage.getRowsTable().should("have.length", 10);
     });
 
-    it("Loads 10 books on pagination size 10", () =>{
-        dashboardPage.getRowsTable().should("have.length", 10);
-    })
+    it("Loads 20 books on pagination size 20", () => {
+      dashboardPage.changePaginationSize("20 / page");
+      dashboardPage.getRowsTable().should("have.length", 20);
+    });
 
     it("Loads all books on pagination size 50", () => {
       dashboardPage.changePaginationSize("50 / page");
-      dashboardPage.getRowsTable().should("have.length", 24);
+      dashboardPage.getRowsTable().should("have.length", 20);
     });
   });
 });
